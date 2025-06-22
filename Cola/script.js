3f1fed148d92da1f46c7e72c1f205ba63c3e720d
Ashis Shrestha <shrestha7.np@gmail.com>
2025-06-21 21:27:08 -0500

    Cola Site

---
+++ Cola/script.js
@@ -0,0 +1,20 @@
+let mainImage = document.getElementById('mainImage');
+let containerDiv = document.getElementById('container');
+function changeOne()
+{
+    mainImage.src = "pepsi001.png";
+    containerDiv.style.backgroundColor = "#3a86ff"; 
+}
+
+function changeTwo()
+{
+    mainImage.src = "pepsi002.png";
+    containerDiv.style.backgroundColor = "#99c1de";
+}
+
+function changeThree()
+{
+    mainImage.src = "pepsi003.png";
+    containerDiv.style.backgroundColor = "#1b263b";
+
+}
